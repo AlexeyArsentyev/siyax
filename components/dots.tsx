ef778330906baf2b31d6a919ffd1bf5b0e8cdfd0
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2024-07-24 10:07:40 +0300

    header and nav fixed

--- components/dots.tsx
+++ components/dots.tsx
@@ -18,7 +18,7 @@ export function HeroHighlightDemo() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl  text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="px-4 text-3xl md:text-4xl lg:text-5xl  text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
         Siya - your only indie&nbsp;
         <Highlight className="text-white">game&nbsp;store</Highlight>
--- components/form.tsx
+++ components/form.tsx
@@ -5,17 +5,35 @@ export function Form() {
     <form className="contact">
       <p>
         <label htmlFor="name">Name:&nbsp;</label>
-        <input type="text" id="name" name="name" placeholder="Somebody" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Somebody "
+          className="active:border-b focus:border-first-100"
+        />
       </p>
       <p>
         <label htmlFor="email">Email:&nbsp;</label>
-        <input type="email" name="email" id="email" placeholder="somebody@gmail.com" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="somebody@gmail.com"
+          className="active:border-b focus:border-third-100"
+        />
       </p>
       <p>
         <label htmlFor="message" className="message-label">
           Message:&nbsp;
         </label>
-        <textarea name="message" id="message" placeholder="Hello, im interested in..."></textarea>
+        <textarea
+          rows={1}
+          name="message"
+          id="message"
+          placeholder="Hello, im interested in..."
+          className="active:border-b focus:border-second-100"
+        ></textarea>
       </p>
     </form>
   );
